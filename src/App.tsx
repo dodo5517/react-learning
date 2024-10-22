a38415f0c6c3b03795f0dd0c4141fc9cc971d0a5
dodo5517 <kh9015548@gmail.com>
2024-10-22 22:40:20 +0900

    글 추가 버튼과 삭제 버튼 추가 및 기능구현

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   let [good, setGood] = useState(new Array(title.length).fill(0)); //[title.length].fill(0) 이렇게 하면 그냥 [3].(0)과 같음.
   let [modal,setmodal] = useState(false);
   let [select, setselect] = useState<number>(0);
+  let [inputValue, setinputValue] = useState<string>('');
 
   return (
       <div className="App">
@@ -24,16 +25,36 @@ function App() {
                   <h4 onClick={()=>{
                       setmodal(!modal); //modal === false ? setmodal(true):setmodal(false);을 간결하게 적을 수 있음.
                       setselect(i);}
-                  }>{title[i]} </h4>
-                  <span onClick={()=>{
-                    let copyGood = [...good]; //state 변경할거니까 copy함.
-                    copyGood[i] = copyGood[i] + 1; //copyGood의 i배열의 값에 +1 함.
-                    setGood(copyGood); //copyGood을 good에 덮음.
-                  }}>굿</span>{good[i]}
+                  }>{title[i]}
+                      <span onClick={(e)=>{
+                          e.stopPropagation(); //이벤트 버블링 막아줌.
+                          let copyGood = [...good]; //state 변경할거니까 copy함.
+                          copyGood[i] = copyGood[i] + 1; //copyGood의 i배열의 값에 +1 함.
+                          setGood(copyGood); //copyGood을 good에 덮음.
+                  }}>굿</span> {good[i]}
+                  </h4>
+                      <button onClick={()=>{
+                          let copy = [...title];
+                          copy.splice(i,1);
+                          setTitle(copy);
+                      }}>삭제</button>
+
                 </div>
             )
           })
         }
+        <div>
+          <input onChange={(e)=>{
+              setinputValue(e.target.value);
+              console.log(inputValue);
+          }}/>
+            <button onClick={()=>{
+                 let newTitle:string[] = [...title];
+                 newTitle.unshift(inputValue);
+                 setTitle(newTitle);
+                console.log(title);
+            }}>글 추가</button>
+        </div>
 
         {
             modal && <Modal select={select} color="yellow" title={title} setTitle={setTitle} good={good}></Modal>
@@ -63,5 +84,6 @@ function Modal(props: ModalProps) {
           </div>
   )
 }
+
 
 export default App;
